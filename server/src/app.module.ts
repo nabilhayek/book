ccb4a15f20bab16536065c78cf7cdd64fcb8e940
Nabil <hayeknabil214@gmail.com>
2023-02-21 00:12:11 +0100

    Prisma, GraphQL & user model init

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,29 @@
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloDriver } from '@nestjs/apollo/dist/drivers';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/types/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+    PrismaModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/user/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserInput, UpdateUserInput } from 'src/types/graphql';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  create(createUserInput: CreateUserInput) {
+    return this.prisma.user.create({
+      data: createUserInput,
+    });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+
+  update(id: string, updateUserInput: UpdateUserInput) {
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserInput,
+    });
+  }
+
+  remove(id: string) {
+    return this.prisma.user.delete({
+      where: { id },
+    });
+  }
+}
