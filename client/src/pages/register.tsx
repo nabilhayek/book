59cbe6202dda6101230bf79c79fa82ba0b28ac42
Nabil <hayeknabil214@gmail.com>
2023-06-22 04:07:38 +0200

    page changes to login & register

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,8 +1,9 @@
 import TextInput from "@/components/TextInput"
 import Button from "@/components/Button"
+import Switch from "@/components/Switch"
 import Link from "next/link"
 
-export default function Login() {
+export default function Register() {
 
 
   type FormEvent = React.FormEvent<HTMLFormElement> & { target: HTMLFormElement & { email: HTMLInputElement, password: HTMLInputElement } }
@@ -19,12 +20,15 @@ export default function Login() {
       <div className="flex flex-col gap-14">
       <form onSubmit={handleSubmit} className="flex flex-col gap-5 w-[300px]">
         <TextInput label="Email" placeholder="Enter your email" type="email" name="email" />
+        <TextInput label="Name" placeholder="Enter your name" type="text" name="name" />
         <TextInput label="Password" placeholder="Enter your password" type="password" name="password" />
-        <Button variant="solid" className="w-full">Login</Button>
+        <TextInput label="Confirm Password" placeholder="Confirm password" type="password" name="confirmPassword" />
+        <Switch label="I agree to the terms and conditions" />
+        <Button variant="solid" className="w-full">Register</Button>
       </form>
-      <Link href="/register">
-      <Button variant="text" className="w-full">Register</Button>
-      </Link>
+        <Link href="/login">
+      <Button variant="text" className="w-full">Login</Button>
+        </Link>
     </div>
     </div>
   )
